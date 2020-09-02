4bca78eb30c12764407e35139e1627fa336c8386
ro walkero <rol1935@protonmail.com>
2020-09-02 16:24:06 +0800

    删除bot token

--- userlistbot.py
+++ userlistbot.py
@@ -1,16 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 """A bot to list all members of a server."""
 import csv
-
+import sys
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-bot = Bot(command_prefix = ">", description='输出服务器用户一览表的bot')
+bot = Bot(command_prefix = ">")
 
 
 @bot.event
 async def on_ready():
-    print('bot登录', )
+    print('bot login>', )
     print('bot.user.name: ', bot.user.name)
     print('bot.user.id: ', bot.user.id)
     print('------')
@@ -40,7 +43,7 @@ async def list(ctx):
 
 @bot.command()
 async def info(ctx):
-    embed = discord.Embed(title="users2csv bot", description="输出服务器的用户一览表", color=0xeee657)
+    embed = discord.Embed(title="users2csv bot", description="export user list", color=0xeee657)
     
     embed.add_field(name="Author", value="walker#9296")
     
@@ -48,6 +51,10 @@ async def info(ctx):
     embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
  
     # give users a link to invite thsi bot to their server
+	
+	
+	
+	
     embed.add_field(name="Invite", value="[Invite link](https://discord.com/api/oauth2/authorize?client_id=750373870745288756&permissions=0&scope=bot)")
  
     await ctx.send(embed=embed)
@@ -57,14 +64,15 @@ bot.remove_command('help')
  
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="users2csv bot", description="输出服务器的用户一览表. 命令:", color=0xeee657)
+    embed = discord.Embed(title="users2csv bot", description="export user list. 命令:", color=0xeee657)
  
-    embed.add_field(name="$list", value="输出服务器的用户一览表", inline=False)
-    embed.add_field(name="$info", value="关于bot的简要信息", inline=False)
-    embed.add_field(name="$help", value="bot的命令帮助信息", inline=False)
+    embed.add_field(name="$list", value="export user list", inline=False)
+    embed.add_field(name="$info", value="about this bot", inline=False)
+    embed.add_field(name="$help", value="bot help", inline=False)
  
     await ctx.send(embed=embed)
 
 
 if __name__ == '__main__':
-    bot.run("NzUwMzczODcwNzQ1Mjg4NzU2.X05mIA.L2wGT3SHkhePCLxS-BRXe5K7aIE") #"把你的Bot的Token放在这里"
+    print(sys.argv[0], sys.argv[1], sys.argv[2])
+    bot.run(sys.argv[1])
